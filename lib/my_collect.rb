59c0b9d9065e346a38d7e4fd13d8d5c6ac9b8c74
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-05 23:43:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_each(array) # put argument(s) here
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection.push(yield(collection[i]))
+    i += 1
+  end
+    new_collection
+  # code here
+end
 
 
